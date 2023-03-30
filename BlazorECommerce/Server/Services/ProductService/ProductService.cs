304f41b8839e8ae3b07d891064c615f2a1e00eb4
Dmitriy Grabovskiy <dimokurl@gmail.com>
2023-03-30 23:06:29 +0300

    Rewrote the GetProdtuctAsync and GetProductsAsync methods. Now we can use the ProductVariants and ProdcutTypes tables inside it.

--- BlazorECommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorECommerce/Server/Services/ProductService/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace BlazorECommerce.Server.Services.ProductService
 {
@@ -14,7 +15,10 @@ namespace BlazorECommerce.Server.Services.ProductService
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _dataContext.Products.FindAsync(productId);
+            var product = await _dataContext.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if(product == null)
             {
                 response.Success = false;
@@ -32,7 +36,9 @@ namespace BlazorECommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _dataContext.Products.ToListAsync(),
+                Data = await _dataContext.Products
+                .Include (p => p.Variants)
+                .ToListAsync(),
                 Success = true
             };
             return response;
@@ -43,7 +49,9 @@ namespace BlazorECommerce.Server.Services.ProductService
             var response = new ServiceResponse<List<Product>>()
             {
                 Data = await _dataContext.Products
-                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl)).ToListAsync(),
+                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl))
+                .Include(p => p.Variants)
+                .ToListAsync(),
                 Success = true
             };
             return response;
