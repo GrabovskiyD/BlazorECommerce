400e2d4097fe1c01f4bc94f6db8eacf8eef49f97
Dmitriy Grabovskiy <dimokurl@gmail.com>
2023-03-30 22:10:29 +0300

    Changed the IProductService in the Client part. Added the OnProductChanged event and than the ProductList component will reload every time when the category changes.

--- BlazorECommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorECommerce/Client/Services/ProductService/ProductService.cs
@@ -3,6 +3,7 @@
     public class ProductService : IProductService
     {
         private readonly HttpClient _httpClient;
+        public event Action? OnProductChanged;
 
         public ProductService(HttpClient httpClient)
         {
@@ -16,13 +17,18 @@
             return result;
         }
 
-        public async Task GetProducts()
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
-            if(result is not null && result.Data is not null)
+            var result = categoryUrl is null ?            
+                await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product") :
+                await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
+
+            if (result is not null && result.Data is not null)
             {
                 Products = result.Data;
             }
+
+            OnProductChanged?.Invoke();
         }
     }
 }
