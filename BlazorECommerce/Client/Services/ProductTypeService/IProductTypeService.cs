dbf29bbb9fd7a8643000a34c35e2e238b00bb825
Dmitriy Grabovskiy <dimokurl@gmail.com>
2023-04-28 23:07:17 +0300

    Implemented Add, Update and Create new product type methods on the client side.

--- BlazorECommerce/Client/Services/ProductTypeService/IProductTypeService.cs
+++ BlazorECommerce/Client/Services/ProductTypeService/IProductTypeService.cs
@@ -5,5 +5,8 @@
         event Action OnChange;
         public List<ProductType> ProductTypes { get; set; }
         Task GetProductTypesAsync();
+        Task AddProductTypesAsync(ProductType productType);
+        Task UpdateProductTypesAsync(ProductType productType);
+        ProductType CreateNewProductType();
     }
 }
--- BlazorECommerce/Client/Services/ProductTypeService/ProductTypeService.cs
+++ BlazorECommerce/Client/Services/ProductTypeService/ProductTypeService.cs
@@ -14,11 +14,37 @@ namespace BlazorECommerce.Client.Services.ProductTypeService
 
         public event Action OnChange;
 
+        public async Task AddProductTypesAsync(ProductType productType)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/ProductType", productType);
+            ProductTypes = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<ProductType>>>()).Data;
+            OnChange?.Invoke();
+        }
+
+        public ProductType CreateNewProductType()
+        {
+            var newProductType = new ProductType
+            {
+                IsNew = true,
+                Editing = true
+            };
+            ProductTypes.Add(newProductType);
+            OnChange?.Invoke();
+            return newProductType;
+        }
+
         public async Task GetProductTypesAsync()
         {
             var result = await _httpClient
                 .GetFromJsonAsync<ServiceResponse<List<ProductType>>>("api/ProductType");
             ProductTypes = result.Data;
+        }
+
+        public async Task UpdateProductTypesAsync(ProductType productType)
+        {
+            var response = await _httpClient.PutAsJsonAsync("api/ProductType", productType);
+            ProductTypes = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<ProductType>>>()).Data;
+            OnChange?.Invoke();
         }
     }
 }
