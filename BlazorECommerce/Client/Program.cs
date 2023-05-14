4a05c31b85e1679504081f6d8b939e1e1f968155
Dmitriy Grabovskiy <dimokurl@gmail.com>
2023-05-14 20:15:22 +0300

    Added MudBlazor to the project.

--- BlazorECommerce/Client/Program.cs
+++ BlazorECommerce/Client/Program.cs
@@ -24,6 +24,7 @@ namespace BlazorECommerce.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
